336f7669d9717a81ca5815eb71651d3e4c681dad
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-28 21:15:28 +0300

    Create ContentView.swift

---
+++ BMIcalculator/BMICalculator/BMICalculator/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BMICalculator
+//
+//  Created by Mariam2 on 12/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
